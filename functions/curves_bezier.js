ee5030d1bd276ec252511cd4e2263adbce126d42
elvis thermo <elvisthermo@hotmail.com>
2019-05-05 16:47:54 -0300

    interface bugadona
    ainda ta incompleta não tem todas as fuções nos butões

--- functions/curves_bezier.js
+++ functions/curves_bezier.js
@@ -1,33 +1,24 @@
-// function bezier(pts) {
-//     return function (t) {
-//         for (var a = pts; a.length > 1; a = b)  // do..while loop in disguise
-//             for (var i = 0, b = [], j; i < a.length - 1; i++)  // cycle over control points
-//                 for (b[i] = [], j = 0; j < a[i].length; j++)  // cycle over dimensions
-//                     b[i][j] = a[i][j] * (1 - t) + a[i+1][j] * t;  // interpolation
-//         return a[0];
-//     }
-// }
-//
-// //
-// // var b = bezier([[10, 30 ],[25, 10],[40, 30]]);
-// // for (var t = 0; t <= 100; t++)
-// //     buffer.putPixel(Math.round(b(t/100)[0]),Math.round(b(t/100)[1]));
-// //
-
-
-
-function bezierPoint(n, controlPt, t){
-    let pts = [];
-    for (let i=0; i <= n; i++)
-        pts.push([controlPt[i][0], controlPt[i][1]]);
-
-    for (let r=1; r <= n; r++) {
-        for (let i=0; i <= n-r; i++) {
-            pts[i][0] = (1-t)*pts[i][0] + t*pts[i+1][0];
-            pts[i][1] = (1-t)*pts[i][1] + t*pts[i+1][1];
+function bezierPoint(p,t,n) {
+    let pts = [...p];
+    for(let r = 1; r <= n; r++) {
+        for (let i = 0; i <= n-r; i++) {
+            // pts[i] = (1-t)*pts[i] + t*pts[i+1];
+            pts[i] = [Math.round((1-t)*pts[i][0] + t*pts[i+1][0]), Math.round((1-t)*pts[i][1] + t*pts[i+1][1])];
         }
     }
+    return [pts[0][0],pts[0][1]];
+}
 
-    buffer.setPoints(pts[0]);
-    return pts[0];
+function bezier(controlPt) {
+    let pts = controlPt, n = pts.length-1;//4
+    // console.log(pts);
+    let points = [];
+    for (let t=0; t<=15; t++) { points.push(bezierPoint(pts,t/15,n));}
+    console.log(points);
+    // bresenham(points);
+
+    for (let i = 0; i <points.length-1 ; i++) {
+        bresenham([points[i],points[i+1]],"black");
+    }
+    return points;
 }
